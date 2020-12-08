614edaf5cf9a8300e8568c9763a3d52595be978d
Luélly Cabral <luellycabral21@gmail.com>
2020-12-08 16:36:19 -0300

    Adiciona a resposta do exercicio

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ultima_Parcela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double ValorTotal;
+            double Parcelas;
+            double Result;
+
+            DateTime DataAtual = DateTime.Now;
+            DateTime ParcelaFinal;
+
+            Console.Write("Digite o seu valor total:  ");
+            ValorTotal = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite seu numero de Parcelas:  ");
+            Parcelas = double.Parse(Console.ReadLine());
+            Console.Write("\n");
+
+            Result = ValorTotal / Parcelas;
+
+            ParcelaFinal = DataAtual.AddMonths(8);
+            Console.WriteLine($"Sua ultima Parcela sera em : {ParcelaFinal}, valor de R${Result}");
+
+
+        }
+    }
+}
